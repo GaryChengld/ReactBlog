6c96a067c915c3e89dfecbc77563b3e4065356b4
GaryChengld <chengld@yahoo.com>
2019-07-27 20:12:17 -0400

    ui

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const db = require('./db/db');
@@ -13,7 +12,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // handle Unauthorized Error
 app.use((err, req, res, next) => {
